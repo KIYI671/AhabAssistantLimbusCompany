54b41db113470bb2d189be9c396dcae19f8243d8
KIYI671 <531585769@qq.com>
2025-01-22 23:38:40 +0800

    测试时注释，忘记取消注释

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import sys
 os.chdir(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False)else os.path.dirname(os.path.abspath(__file__)))
 
 # 获取管理员权限
-"""import pyuac
+import pyuac
 if not pyuac.isUserAdmin():
     try:
         pyuac.runAsAdmin(False)
         sys.exit(0)
     except Exception:
-        sys.exit(1)"""
+        sys.exit(1)
 
 from win32api import GetLastError
 from win32event import CreateMutex
