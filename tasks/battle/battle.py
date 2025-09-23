059903af8cce352881a671f3fb2e65492d8ca9c4
KIYI671 <531585769@qq.com>
2025-09-24 07:37:56 +0800

    Update battle.py
    处理缩进

--- tasks/battle/battle.py
+++ tasks/battle/battle.py
@@ -85,9 +85,9 @@ class Battle:
             if self.mouse_click_rate:
                 my_scale = cfg.set_win_size / 1440
                 if pos:= auto.find_element("battle/win_rate_card.png"):
-                   pos = [pos[0] + 50 * my_scale, pos[1] - 50 * my_scale]
-                   auto.mouse_click(pos[0], pos[1])
-                   auto.click_element("battle/gear_right.png")
+                    pos = [pos[0] + 50 * my_scale, pos[1] - 50 * my_scale]
+                    auto.mouse_click(pos[0], pos[1])
+                    auto.click_element("battle/gear_right.png")
             else:
                 sleep(1)
                 if not auto.find_element("battle/pause_assets.png", threshold=0.75):
