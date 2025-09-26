b4dc08df48726d12b516380a92d2f34094b38912
233dada <2474129326@qq.com>
2025-09-26 15:34:22 +0800

    fix: 为部分文件的日志函数增加了堆栈等级, 以符合先前的日志堆栈记录行为 同时将logger不再作为参数传入 而是改为模块导入

--- module/automation/__init__.py
+++ module/automation/__init__.py
@@ -1,6 +1,5 @@
 from module.automation.automation import Automation
 from module.automation.screenshot import ScreenShot
 from module.config import cfg
-from module.logger import log
 
-auto = Automation(cfg.get_value('game_title_name'), log)
+auto = Automation(cfg.get_value('game_title_name'))
