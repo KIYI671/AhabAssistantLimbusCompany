4ca71f37e19b546940076963752960cf2ad17b1d
NaNExist <138002947+NaNExist@users.noreply.github.com>
2025-10-11 14:38:27 +0800

    Feat:增加了截图工具的弹窗显示

--- tasks/tools/screenshot_module.py
+++ tasks/tools/screenshot_module.py
@@ -3,6 +3,10 @@ from module.config import cfg
 from module.logger import log
 from module.game_and_screen import screen
 from PySide6.QtCore import QThread
+from app.card.messagebox_custom import BaseInfoBar
+from PySide6.QtCore import QT_TRANSLATE_NOOP, Qt
+from qfluentwidgets import FluentIcon as FIF, InfoBarPosition
+from qfluentwidgets import ScrollArea
 import time
 
 class ScreenshotGet(QThread):
@@ -24,5 +28,13 @@ class ScreenshotGet(QThread):
             log.error(f"截图错误: {str(e)}")
             return
         screen.reset_win()
-        log.info("截图已保存")
+        bar = BaseInfoBar.success(
+            title=QT_TRANSLATE_NOOP("BaseInfoBar", '截图完成'),
+            content=f'图片保存为 screenshot_{timestr}.png',
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_RIGHT,
+            duration=5000,
+            parent=None # Fixme: parent应该绑在什么上面
+        )
 
