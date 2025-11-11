a3660cff35e0ff12aae67ee820292635258d2941
KIYI671 <531585769@qq.com>
2025-11-11 23:41:31 +0800

    ⚙️ chore(admin): 恢复管理员权限获取逻辑
    【恢复】取消注释 main.py 中的管理员权限获取代码块，确保程序在需要时自动提权运行。

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from module.config import cfg
 os.chdir(
     os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__)))
 
-# # 获取管理员权限
-# import pyuac
-#
-# if not pyuac.isUserAdmin():
-#     try:
-#         pyuac.runAsAdmin(False)
-#         sys.exit(0)
-#     except Exception:
-#         sys.exit(1)
+# 获取管理员权限
+import pyuac
+
+if not pyuac.isUserAdmin():
+    try:
+        pyuac.runAsAdmin(False)
+        sys.exit(0)
+    except Exception:
+        sys.exit(1)
 
 from win32api import GetLastError
 from win32event import CreateMutex
