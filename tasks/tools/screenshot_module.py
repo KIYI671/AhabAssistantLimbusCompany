3226a3a412bbeecdedfef8964f7456ffcda15d4f
233dada <233dada@gmail.com>
2026-02-09 16:36:02 +0800

    fix: 修复当游戏未启动时, 截图测试的时间戳不一致的问题
    部分代码在上个提交

--- tasks/tools/screenshot_module.py
+++ tasks/tools/screenshot_module.py
@@ -1,15 +1,17 @@
 import time
 
-from PySide6.QtCore import QThread
-
+from PySide6.QtCore import QThread, Signal
 from module.automation import auto
 from module.game_and_screen import screen
 from module.logger import log
 
 
 class ScreenshotGet(QThread):
+    on_saved_timestr = Signal(str)
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.finished.connect(self.deleteLater)
 
     def run(self):
         try:
@@ -25,6 +27,7 @@ class ScreenshotGet(QThread):
                 timestr = time.strftime("%Y%m%d_%H%M%S", time.localtime())
                 img.save(f"screenshot_{timestr}.png")
                 log.info(f"图片保存为 AALC > screenshot_{timestr}.png")
+                self.on_saved_timestr.emit(timestr)
             else:
                 log.error("截图失败，请确认游戏是否处于启动状态")
         except Exception as e:
