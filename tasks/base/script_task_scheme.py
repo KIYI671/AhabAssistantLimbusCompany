dc88aae0418e8c67058b48c48b946d37ad498734
233dada <233dada@gmail.com>
2026-03-07 07:27:30 +0800

    chore: 为 windows 添加ui警告, 同时为警告部分增加非模拟器判断
    要游戏关闭设置才会生效

--- tasks/base/script_task_scheme.py
+++ tasks/base/script_task_scheme.py
@@ -293,11 +293,17 @@ def script_task() -> None | int:
         log.error(f"自动切换语言出错: {e}，使用英语尝试")
         cfg.set_value("language_in_game", "en")
 
-    # 低渲染比例发出警告
-    if get_game_config_from_registry().get("_renderingScale", -1) == 2:
-        log.warning("当前游戏渲染比例为低, 可能会导致识别错误, 建议设置为中或更高")
-    if cfg.set_win_size == 720:
-        log.warning("当前游戏分辨率为1280*720, 可能会导致识别错误或卡死, 建议设置为更高分辨率")
+    if not cfg.simulator:
+
+        # 低渲染比例发出警告
+        if get_game_config_from_registry().get("_renderingScale", -1) == 2:
+            log.warning("当前游戏渲染比例为低, 可能会导致识别错误, 建议设置为中或更高")
+        if cfg.set_win_size == 720:
+            log.warning("当前游戏分辨率为1280*720, 可能会导致识别错误或卡死, 建议设置为更高分辨率")
+
+        # 非默认ui发出警告
+        if get_game_config_from_registry().get("_duiTheme", -1) != 0:
+            log.warning("当前游戏UI为非亮色UI, 可能会导致识别错误, 建议设置为亮色(基础)UI")
 
     if cfg.language_in_game == "zh_cn" and pic_path[0] != "zh_cn":
         pic_path.insert(0, "zh_cn")
