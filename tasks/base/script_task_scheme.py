f35f37b62a655560ecb77ec44bab50fa70f01ece
lushuo <luyipao6@gmail.com>
2026-04-04 23:51:34 +0800

    fix: 修复pic_path没有正确重置的问题

--- tasks/base/script_task_scheme.py
+++ tasks/base/script_task_scheme.py
@@ -302,42 +302,44 @@ def script_task() -> None | int:
         if cfg.set_win_size == 720:
             log.warning("当前游戏分辨率为1280*720, 可能会导致识别错误或卡死, 建议设置为更高分辨率")
 
-    if cfg.language_in_game == "zh_cn" and pic_path[0] != "zh_cn":
-        pic_path.insert(0, "zh_cn")
+    pic_path.clear()
+    if cfg.language_in_game == "zh_cn":
+        pic_path.extend(["zh_cn", "en", "share"])
     elif cfg.language_in_game == "en":
-        while pic_path[0] != "share":
-            pic_path.pop(0)
-        pic_path.insert(0, "en")
+        pic_path.extend(["en", "share"])
 
-    # 检查主题色
+    # 检查主题
     theme_detected = None
     for loop_idx in range(2):
+        log.info(f"正在检测主题, 第{loop_idx + 1}次")
         if loop_idx == 0:
             for _ in range(3):
                 auto.key_press("esc")
-                sleep(0.2)
-            sleep(0.5)
+                sleep(0.5)
         else:
             auto.key_press("esc")
             sleep(0.5)
 
+        auto.clear_img_cache()
         auto.take_screenshot()
-
         if (
-            auto.find_element("battle/give_up_assets.png", model="normal")
-            or auto.find_element("mirror/road_in_mir/to_window_assets.png", model="normal")
-            or auto.find_element("home/window_assets.png", model="normal")
+            auto.find_element("battle/give_up_assets.png")
+            or auto.find_element("battle/normal_give_up_assets.png")
+            or auto.find_element("mirror/road_in_mir/to_window_assets.png")
+            or auto.find_element("home/window_assets.png")
         ):
             log.info("当前主题为: default")
             theme_detected = "default"
             break
 
         pic_path.insert(0, "share_dark")
+        auto.clear_img_cache()
         try:
             is_dark = (
-                auto.find_element("battle/give_up_assets.png", model="normal")
-                or auto.find_element("mirror/road_in_mir/to_window_assets.png", model="normal")
-                or auto.find_element("home/window_assets.png", model="normal")
+                auto.find_element("battle/give_up_assets.png")
+                or auto.find_element("battle/normal_give_up_assets.png")
+                or auto.find_element("mirror/road_in_mir/to_window_assets.png")
+                or auto.find_element("home/window_assets.png")
             )
             if is_dark:
                 log.info("当前主题为: dark")
@@ -353,7 +355,7 @@ def script_task() -> None | int:
     if theme_detected == "dark":
         pic_path.clear()
         if cfg.language_in_game == "zh_cn":
-            pic_path.extend(["zh_cn_dark", "zh_cn", "share_dark", "share"])
+            pic_path.extend(["zh_cn_dark", "zh_cn","en_dark", "en", "share_dark", "share"])
         else:
             pic_path.extend(["en_dark", "en", "share_dark", "share"])
 
